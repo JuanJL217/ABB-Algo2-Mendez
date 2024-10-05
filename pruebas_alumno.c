bde0f1915b25ea8caf499d805ea59ce613f757f9
JuanJL217 <juandocumentos217@gmail.com>
2024-10-05 01:38:30 -0300

    Doble puntero OK

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,155 @@
 #include "pa2m.h"
+#include "src/abb.h"
 
-void prueba_simple()
+int comparar_numeros(void* a, void* b)
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i == j, "i es igual a j (%d == %d)", i, j);
-	pa2m_afirmar(2*i == 10, "2*i es igual a 2 (%d)", 2*i);
+	return *(int*)a - *(int*)b;
+}
+
+void inicializarAbb()
+{
+	abb_t* abb = abb_crear(comparar_numeros);
+	if (!abb)
+		return;
+	int numero = 5;
+	pa2m_afirmar(abb_cantidad(abb) == 0, "ABB inicializado tiene 0 elemento");
+	if (!abb_insertar(abb, (void*)&numero)) {
+		abb_destruir(abb);
+		return;
+	}
+	pa2m_afirmar(abb_cantidad(abb) == 1, "Se agregó satisfactoriamene un elemento");
+	abb_destruir(abb);
+	
+}
+
+void agregarElementosDiferentes()
+{
+	abb_t* abb = abb_crear(comparar_numeros);
+	if (!abb)
+		return;
+	
+	int numeros[] = {6, 2, 7, 1, 8, 9};
+	for (size_t i = 0; i < 6; i++) {
+		if (!abb_insertar(abb, (void*)&numeros[i])) {
+			abb_destruir(abb);
+			return;
+		}
+	}
+	pa2m_afirmar(abb_cantidad(abb) == 6, "Se agregaron 6 elementos diferentes en el abb");
+	abb_destruir_todo(abb, NULL);
+}
+
+void agregarTodosRepetidos()
+{
+	abb_t* abb = abb_crear(comparar_numeros);
+	if (!abb)
+		return;
+	
+	int numeros[] = {6, 6, 6, 6, 6, 6, 6, 6, 6, 6};
+	for (size_t i = 0; i < 10; i++) {
+		if (!abb_insertar(abb, (void*)&numeros[i])) {
+			abb_destruir(abb);
+			return;
+		}
+	}
+	pa2m_afirmar(abb_cantidad(abb) == 10, "Se agregaron 10 elementos repetidos en el abb");
+	abb_destruir_todo(abb, NULL);
+}
+
+void agregarElementosVariados()
+{	
+	abb_t* abb = abb_crear(comparar_numeros);
+	int numeros[] = {10, 2, 2, 6, 9, 2, 14, 10, 17, 6, 1, 1, 1, 9, 2, 14};
+	for (size_t i = 0; i < 16; i++){
+		if (!abb_insertar(abb, (void*)&numeros[i])) {
+			abb_destruir(abb);
+			return;
+		}
+	}
+	pa2m_afirmar(abb_cantidad(abb) == 16, "Se agregaron 16 elementos, entre repetidos y diferentes en el abb");
+	abb_destruir(abb);
+}
+void obtenerElementos()
+{
+	abb_t* abb = abb_crear(comparar_numeros);
+	if (!abb)
+		return;
+	
+	int numeros[] = {10, -4, 2, 5, 8, 1, 13, -9};
+	for (size_t i = 0; i < 8; i++) {
+		if (!abb_insertar(abb, (void*)&numeros[i])) {
+			abb_destruir(abb);
+			return;
+		}
+	}
+	pa2m_afirmar(abb_cantidad(abb) == 8, "Se agregaron 8 elementos");
+
+	int numeros_a_buscar[] = {-9, 13, 1, 8, 5, 2, -4, 10};
+	int* obtenido;
+	for (size_t i = 0; i < 8; i++) {
+		obtenido = (int*) abb_obtener(abb, (void*)&numeros_a_buscar[i]);
+		pa2m_afirmar(obtenido == &numeros[7-i], "Se encontró el elemento '%d', direccion obtenida: '%p' -> direccion esperada: '%p'", numeros[7-i], obtenido, &numeros[7-i]);
+	}
+	int buscar = 20;
+	obtenido = (int*) abb_obtener(abb, (void*)&buscar);
+	pa2m_afirmar(obtenido == NULL, "Buscar un elemento que no está en el abb, retorna NULL");
+
+	abb_destruir_todo(abb, NULL);
+}
+
+void agregarMismoElemento()
+{
+	return;
+}
+
+bool mostrar_numeros(void* elemento, void* nada)
+{	
+	printf("%d ", *(int*)elemento);
+	if (*(int*)elemento == 2)
+		return false;
+	return true;
+}
+
+void iteradorIternoSinCorte()
+{
+	return;
+}
+
+void iteradorIternoConCorte()
+{
+	abb_t* abb = abb_crear(comparar_numeros);
+	if (!abb)
+		return;
+	
+	int numeros[] = {10, -4, 2, 5, 8, 1, 13, -9};
+	for (size_t i = 0; i < 8; i++) {
+		if (!abb_insertar(abb, (void*)&numeros[i])) {
+			abb_destruir(abb);
+			return;
+		}
+	}
+	pa2m_afirmar(abb_cantidad(abb) == 8, "Se agregaron 8 elementos");
+	size_t cantidad = abb_iterar_postorden(abb, mostrar_numeros, NULL);
+	pa2m_afirmar(cantidad == 5, "Se recorrió solo 5 elementos");
+	abb_destruir(abb);
 }
 
 int main()
 {
-	pa2m_nuevo_grupo("============== ??? ===============");
-	prueba_simple();
+	pa2m_nuevo_grupo("============== PRUEBA INICIALIZAR ABB ===============");
+	inicializarAbb();
+	pa2m_nuevo_grupo("============== PRUEBA AGREGAR ELEMENTOS AL ABB ===============");
+	agregarElementosDiferentes();
+	agregarTodosRepetidos();
+	agregarElementosVariados();
+	pa2m_nuevo_grupo("============== PRUEBA OBTENER ELEMENTOS ===============");
+	obtenerElementos();
+
+	pa2m_nuevo_grupo("============== PRUEBA ITERADOR POSTORDEN ==============");
+	iteradorIternoSinCorte();
+	iteradorIternoConCorte();
+	// pa2m_nuevo_grupo("============== PRUEBA AGREGAR MISMO ELEMENTO ===============");
+	// agregarMismoElemento();
 
 	return pa2m_mostrar_reporte();
 }
--- src/abb.c
+++ src/abb.c
@@ -1,6 +1,28 @@
 #include "abb.h"
 #include "abb_estructura_privada.h"
 
+nodo_t* nodo_crear(void* elemento)
+{
+    nodo_t* nodo = malloc(sizeof(nodo_t));
+    if (!nodo)
+        return NULL;
+    nodo->elemento = elemento;
+    nodo->izq = NULL;
+    nodo->der = NULL;
+    return nodo;
+}
+
+nodo_t** buscar_nodo(abb_t* abb, nodo_t** nodo_actual, void* elemento)
+{
+    if (!*nodo_actual || abb->comparador(elemento, (*nodo_actual)->elemento) == 0) {
+        return nodo_actual;
+    } else if (abb->comparador(elemento, (*nodo_actual)->elemento) < 0) {
+        return buscar_nodo(abb, &(*nodo_actual)->izq, elemento);
+    } else {
+        return buscar_nodo(abb, &(*nodo_actual)->der, elemento);
+    }
+}
+
 abb_t* abb_crear(int (*comparador)(void*, void*))
 {
     abb_t* inicializar_abb = malloc(sizeof(abb_t));
@@ -12,67 +34,164 @@ abb_t* abb_crear(int (*comparador)(void*, void*))
     return inicializar_abb;
 }
 
-
-void abb_destruir(abb_t* abb)
+void destruir_todo_el_abb(nodo_t* nodo_actual, void (*destructor)(void*))
 {
-    return;
+    //POSTORDEN
+    if (!nodo_actual)
+        return;
+    destruir_todo_el_abb(nodo_actual->izq, destructor);
+    destruir_todo_el_abb(nodo_actual->der, destructor);
+    if (destructor)
+        destructor(nodo_actual->elemento);
+    free(nodo_actual);
 }
-
 
 void abb_destruir_todo(abb_t* abb, void (*destructor)(void*))
 {
-    return;
+    if (!abb)
+        return;
+    destruir_todo_el_abb(abb->raiz, destructor);
+    free(abb);
+}
+
+void abb_destruir(abb_t* abb)
+{
+    if (!abb)
+        return;
+    abb_destruir_todo(abb, NULL);
+}
+
+nodo_t** buscar_predecesor_inorden(nodo_t** nodo_actual)
+{   
+    if (!*nodo_actual) {
+        return nodo_actual;
+    }
+    return buscar_predecesor_inorden(&((*nodo_actual)->der));
+}
+
+bool abb_esta_vacio(abb_t* abb)
+{
+    return abb->nodos == 0;
 }
 
 bool abb_insertar(abb_t *abb, void* elemento)
 {
+    if (!abb)
+        return false;
+
+    nodo_t* nuevo_nodo = nodo_crear(elemento);
+    if (!nuevo_nodo) 
+        return false;
+
+    if (abb_esta_vacio(abb)) {
+        abb->raiz = nuevo_nodo;
+    } else {
+        nodo_t** nodo_padre = buscar_nodo(abb, &(abb->raiz), elemento);
+        if (!*nodo_padre) {
+            (*nodo_padre) = nuevo_nodo;
+        } else {
+            nodo_t** nodo_encontrado = buscar_predecesor_inorden(&((*nodo_padre)->izq));
+            (*nodo_encontrado) = nuevo_nodo;
+        }
+    }
+    abb->nodos++;
     return true;
 }
 
 
-bool abb_quitar(abb_t* abb, void* buscado, void** encontrado)
-{
-    return true;
-}
+
+// bool abb_quitar(abb_t* abb, void* buscado, void** encontrado)
+// {   
+//     if (!abb || !abb->raiz)
+//         return false;
+    
+//     nodo_t* nodo_eliminar = buscar_nodo(abb, &(abb->raiz), buscado);
+//     if (!nodo_eliminar)
+//         return false;
+
+//     void* elemento_encontrado = nodo_eliminar->elemento;
+
+//     // los casos ...
+
+
+//     if (encontrado)
+//         *encontrado = elemento_encontrado;
+//     return true;
+// }
 
 
 void* abb_obtener(abb_t* abb, void* elemento)
 {
-    return;
+    if (!abb || !abb->raiz)
+        return NULL;
+    
+    nodo_t** nodo_obtenido = buscar_nodo(abb, &(abb->raiz), elemento);
+    if (!*nodo_obtenido)
+        return NULL;
+    
+    return (*nodo_obtenido)->elemento;
 }
 
 
 size_t abb_cantidad(abb_t* abb)
 {
-    return 0;
+    return abb->nodos;
 }
 
+// void recorrido_inorden(nodo_t* nodo_actual, bool (*f)(void*, void*), void* ctx, size_t* contador)
+// {  
+//     if !()
+// }
 
-size_t abb_iterar_inorden(abb_t* abb, bool (*f)(void*,void*), void* ctx)
+// size_t abb_iterar_inorden(abb_t* abb, bool (*f)(void*,void*), void* ctx)
+// {
+//     if(!abb || !abb->raiz)
+//         return 0;
+//     size_t cantidad_nodos_recorridos = 0;
+//     recorrido_inorden(abb->raiz, f, &cantidad_nodos_recorridos);
+//     return cantidad_nodos_recorridos;
+// }
+// size_t abb_iterar_preorden(abb_t* abb, bool (*f)(void*,void*), void* ctx)
+// {
+//     return 0;
+// }
+
+
+bool recorrido_postorden(nodo_t* nodo_actual, bool (*f)(void*, void*), void* ctx, size_t* contador)
 {
-    return 0;
+    if (!nodo_actual)
+        return true;
+    if (!recorrido_postorden(nodo_actual->izq, f, ctx, contador) || !recorrido_postorden(nodo_actual->der, f, ctx, contador))
+        return false;
+    if (!f(nodo_actual->elemento, ctx))
+        return false;
+    (*contador)++;
+    return true;
 }
-size_t abb_iterar_preorden(abb_t* abb, bool (*f)(void*,void*), void* ctx)
-{
-    return 0;
-}
+
 
 size_t abb_iterar_postorden(abb_t* abb, bool (*f)(void*,void*), void* ctx)
 {
-    return 0;
+    if (!abb)
+        return 0;
+    
+    size_t cantidad_nodos_recorridos = 0;
+    if (!recorrido_postorden(abb->raiz, f, ctx, &cantidad_nodos_recorridos))
+        return cantidad_nodos_recorridos+1;
+    return abb->nodos;
 }
 
-size_t abb_vectorizar_inorden(abb_t* abb, void** vector, size_t tamaño)
-{
-    return 0;
-}
+// size_t abb_vectorizar_inorden(abb_t* abb, void** vector, size_t tamaño)
+// {
+//     return 0;
+// }
 
-size_t abb_vectorizar_preorden(abb_t* abb, void** vector, size_t tamaño)
-{
-    return 0;
-}
+// size_t abb_vectorizar_preorden(abb_t* abb, void** vector, size_t tamaño)
+// {
+//     return 0;
+// }
 
-size_t abb_vectorizar_postorden(abb_t* abb, void** vector, size_t tamaño)
-{
-    return 0;
-}
+// size_t abb_vectorizar_postorden(abb_t* abb, void** vector, size_t tamaño)
+// {
+//     return 0;
+// }
